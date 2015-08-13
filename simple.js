8315c272eaa3296ac4ec887a75540938e0147d05
910JQK <v910JQK@gmail.com>
2015-08-13 23:51:26 +0800

    Fix a bug and add comments

--- simple.js
+++ simple.js
@@ -1,28 +1,55 @@
 'use strict';
 
 
+/**
+ * Alias of document.querySelector()
+ * @param String selector
+ * @return Element
+ */
 function $(selector){
     return document.querySelector(selector);
 }
 
 
+/**
+ * Alias of document.querySelectorAll()
+ * @param String selector
+ * @return NodeList
+ */
 function $All(selector){
     return document.querySelectorAll(selector);
 }
 
 
+/**
+ * Assigns elements matched by selector to global variables.
+ * @param Object list
+ * @return void
+ */
 function assignGlobalObjects(list){
     for(let item of Object.keys(list))
 	window[item] = $(list[item]);
 }
 
 
+/**
+ * Adds methods to a class by code written in constructor (for ES5-way).
+ * @param Function constructor
+ * @param Object methods
+ * @return void
+ */
 function assignMethods(constructor, methods){
     for(let I of Object.keys(methods))
 	constructor.prototype[I] = methods[I];
 }
 
 
+/**
+ * Sets properties of the specified html element.
+ * @param HTMLElement element
+ * @param String|Array|Object properties
+ * @return void
+ */
 function elementAssign(element, properties){
     /* no property is set, exit without processing */
     if(!properties)
@@ -79,6 +106,12 @@ function elementAssign(element, properties){
 }
 
 
+/**
+ * Creates a html element with specified value of properties.
+ * @param String type
+ * @param String|Array|Object properties
+ * @return HTMLElement
+ */
 function create(type, properties){
     var element = document.createElement(type);
     elementAssign(element, properties);
@@ -86,14 +119,26 @@ function create(type, properties){
 }
 
 
+/**
+ * Creates an instance of specified template and fill it with data.
+ * @param HTMLTemplateElement template
+ * @param Object data
+ * @return DocumentFragment
+ */
 function inst(template, data){
     var instance = document.importNode(template, true).content;
-    for(let selector of Object.keys(data))
-	elementAssign(instance.querySelector(selector), data[selector]);
+    if(data)
+	for(let selector of Object.keys(data))
+	    elementAssign(instance.querySelector(selector), data[selector]);
     return instance;
 }
 
 
+/**
+ * Sets CSS property "display" of the specified html element to none.
+ * @param HTMLElement element
+ * @return Boolean
+ */
 function hide(element){
     if(element.style.display == 'none')
 	return false;
@@ -104,6 +149,11 @@ function hide(element){
 }
 
 
+/**
+ * Restores the specified hidden element.
+ * @param HTMLElement element
+ * @return Boolean
+ */
 function show(element){
     if(element.style.display != 'none')
 	return false;
@@ -117,16 +167,32 @@ function show(element){
 }
 
 
+/**
+ * Inserts "newNode" before "referenceNode".
+ * @param Node newNode
+ * @param Node referenceNode
+ * @return void
+ */
 function insertBefore(newNode, referenceNode){
     referenceNode.parentNode.insertBefore(newNode, referenceNode);
 }
 
 
+/**
+ * Inserts "newNode" after "referenceNode".
+ * @param Node newNode
+ * @param Node referenceNode
+ * @return void
+ */
 function insertAfter(newNode, referenceNode){
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 
+/**
+ * Iterator of siblings of "element" toward the first child of its parent.
+ * @param Element element
+ */
 function* previousElementIterator(element){
     while(element){
 	element = element.previousElementSibling;
@@ -137,6 +203,10 @@ function* previousElementIterator(element){
 }
 
 
+/**
+ * Iterator of siblings of "element" toward the last child of its parent.
+ * @param Element element
+ */
 function* nextElementIterator(element){
     while(element){
 	element = element.nextElementSibling;
@@ -147,6 +217,11 @@ function* nextElementIterator(element){
 }
 
 
+/**
+ * Fetches data of the specified form to an object (key-value pairs).
+ * @param HTMLFormElement form
+ * @return Object
+ */
 function fetchFormData(form){
     var data = {};
     var input_elements = form.querySelectorAll('input');
@@ -162,6 +237,12 @@ function fetchFormData(form){
 }
 
 
+/**
+ * Fills the specified form with data of the key-value pairs.
+ * @param HTMLFormElement form
+ * @param Object data
+ * @return void
+ */
 function fillForm(form, data){
     for(let I of Object.keys(data)){
 	let input = form.querySelector(printf('input[name="%1"]', I));
@@ -173,18 +254,34 @@ function fillForm(form, data){
 }
 
 
+/**
+ * Disables the specified form.
+ * @param HTMLFormElement form
+ * @return void
+ */
 function disableForm(form){
     for(let I of form.querySelectorAll('input'))
 	I.disabled = true;
 }
 
 
+/**
+ * Enables the specified form.
+ * @param HTMLFormElement form
+ * @return void
+ */
 function enableForm(form){
     for(let I of form.querySelectorAll('input'))
 	I.disabled = false;
 }
 
 
+/**
+ * Returns a copy of "str" with placeholders replaced by the rest arguments.
+ * @param String str
+ * @param String ...args
+ * @return String
+ */
 function printf(args){
     var string = arguments[0];
     /* note that %n in the string must be in ascending order */
@@ -196,6 +293,11 @@ function printf(args){
 }
 
 
+/**
+ * Returns a deep-clone of the specified object.
+ * @param Object obj
+ * @return Object
+ */
 function clone(obj){
     /* code from stackoverflow #122102, much faster than other methods
      * modified for only cloning simple objects which only contain
@@ -220,7 +322,7 @@ function clone(obj){
 }
 
 
-/* Polyfill */
+/* Polyfill NodeList.prototype[Symbol.iterator] */
 if(!NodeList.prototype[Symbol.iterator]){
     NodeList.prototype[Symbol.iterator] = function* (){
 	for(let i=0; i<this.length; i++)
